152ac36c283b2481e0269db714ee911b7e0fc47c
Superingxz <1803117759@qq.com>
2016-12-27 23:21:00 +0800

    CoordinatorLayout例子

---
+++ app/src/main/java/org/mobiletrain/a8_3coordinatorlayout/MainActivity.java
@@ -0,0 +1,22 @@
+package org.mobiletrain.a8_3coordinatorlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void fabClick(View view) {
+        Toast.makeText(MainActivity.this, "123", Toast.LENGTH_SHORT).show();
+    }
+}
